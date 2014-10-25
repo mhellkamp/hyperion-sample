1b99d70c59dab52d7a7394ba7d64a11e76efdebf
Mark Hellkamp <mark.hellkamp@gmail.com>
2014-10-25 15:01:33 -0700

    Updates

--- src/main/java/com/dottydingo/hyperion/northwind/service/model/PersistentHistoryEntry.java
+++ src/main/java/com/dottydingo/hyperion/northwind/service/model/PersistentHistoryEntry.java
@@ -1,17 +1,76 @@
 package com.dottydingo.hyperion.northwind.service.model;
 
-import com.dottydingo.hyperion.jpa.model.DefaultPersistentHistoryEntry;
+import com.dottydingo.hyperion.api.HistoryAction;
+import com.dottydingo.hyperion.jpa.model.BasePersistentHistoryEntry;
 
-import javax.persistence.Access;
-import javax.persistence.AccessType;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 /**
  */
 @Entity
-@Table(name="entity_history")
+@Table(name = "entity_history")
 @Access(AccessType.PROPERTY)
-public class PersistentHistoryEntry extends DefaultPersistentHistoryEntry<Long>
+public class PersistentHistoryEntry extends BasePersistentHistoryEntry<Long>
 {
+    @Id()
+    @GeneratedValue
+    @Column(name = "entity_history_id")
+    @Override
+    public Long getId()
+    {
+        return super.getId();
+    }
+
+    @Column(name = "entity_type")
+    @Override
+    public String getEntityType()
+    {
+        return super.getEntityType();
+    }
+
+    @Column(name = "entity_id")
+    @Override
+    public Long getEntityId()
+    {
+        return super.getEntityId();
+    }
+
+    @Column(name = "history_action")
+    @Enumerated(EnumType.STRING)
+    @Override
+    public HistoryAction getHistoryAction()
+    {
+        return super.getHistoryAction();
+    }
+
+    @Column(name = "api_version")
+    @Override
+    public Integer getApiVersion()
+    {
+        return super.getApiVersion();
+    }
+
+    @Column(name = "serialized_entry")
+    @Lob
+    @Override
+    public String getSerializedEntry()
+    {
+        return super.getSerializedEntry();
+    }
+
+    @Column(name = "user")
+    @Override
+    public String getUser()
+    {
+        return super.getUser();
+    }
+
+    @Column(name = "timestamp")
+    @Temporal(TemporalType.TIMESTAMP)
+    @Override
+    public Date getTimestamp()
+    {
+        return super.getTimestamp();
+    }
 }
